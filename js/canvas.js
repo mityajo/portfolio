7fe4ea50b13d293403ca11a3c6b1ec7806a0de22
Roman <mityagin86@gmail.com>
2023-01-05 20:25:11 +0700

    Finished Canvas Particles

--- js/canvas.js
+++ js/canvas.js
@@ -7,8 +7,9 @@
         properties = {
             bgColor: 'rgba(37, 41, 52, 1)',
             particleColor: 'rgba(4, 194, 201, 1)',
-            particleRadius: 3,
+            particleRadius: 2,
             particleCount: 60,
+            particleLife: 10,
             particleMaxVelocity: 0.5,
             lineLength: 150
         };
@@ -24,21 +25,30 @@
             this.y = Math.random() * h;
             this.velocityX = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
             this.velocityY = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
+            this.life = Math.random() * properties.particleLife * 60;
         }
-
         position() {
             this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX < 0 ? this.velocityX *= -1 : this.velocityX;
             this.y + this.velocityY > h && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY < 0 ? this.velocityY *= -1 : this.velocityY;
             this.x += this.velocityX;
             this.y += this.velocityY;
         }
-
         reDraw() {
             ctx.beginPath();
             ctx.arc(this.x, this.y, properties.particleRadius, 0, Math.PI * 2);
             ctx.closePath();
             ctx.fillStyle = properties.particleColor;
             ctx.fill();
+        }
+        reCalculateLife() {
+            if (this.life < 1) {
+                this.x = Math.random() * w;
+                this.y = Math.random() * h;
+                this.velocityX = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
+                this.velocityY = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
+                this.life = Math.random() * properties.particleLife * 60;
+            }
+            this.life--;
         }
     }
 
@@ -57,21 +67,22 @@
                 y2 = particles[j].y;
                 length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
                 if (length < properties.lineLength) {
+                    opacity = 1 - length / properties.lineLength;
                     ctx.lineWidth = '0.5';
-                    ctx.strokeStyle = 'rgba(4, 194, 201, 1)';
+                    ctx.strokeStyle = 'rgba(4, 194, 201, ' + opacity + ')';
                     ctx.beginPath();
                     ctx.moveTo(x1, y1);
                     ctx.lineTo(x2, y2);
                     ctx.closePath();
                     ctx.stroke();
                 }
-
             }
         }
     }
 
     function reDrawParticles() {
         for (let i in particles) {
+            particles[i].reCalculateLife();
             particles[i].position();
             particles[i].reDraw();
         }
